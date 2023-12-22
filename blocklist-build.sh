6da0faf4d4a449d8b886599f7ad465af9a3fc25f
tysliu <68217+tysliu@users.noreply.github.com>
2023-12-22 14:24:47 +0800

    Update blocklist-build.sh
    skip countries

--- blocklist-build.sh
+++ blocklist-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl -s https://www.iblocklist.com/lists.json | \
-    jq -r '.lists[] | .name as $n | select((.subscription == "false") and ($n | startswith("iana-") | not) and (["fornonlancomputers", "bogon", "The Onion Router"] | index($n) | not)) | .list' | \
+    jq -r '.lists[] | .name as $n | .list as $l | select((.subscription == "false") and ($n | startswith("iana-") | not) and (["fornonlancomputers", "bogon", "The Onion Router"] | index($n) | not) and ($l | length > 2)) | .list' | \
     awk 'length($0) > 2 { print "http://list.iblocklist.com/?fileformat=p2p&archiveformat=gz&list=" $0 }' | \
     xargs wget -O - | \
     gunzip | \
